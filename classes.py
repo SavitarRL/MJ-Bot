1eb3c601403391774cd7e4e4e7d55c4a882bf3cd
unknown <ryanlaupakto2000@gmail.com>
2020-07-01 18:10:22 +0800

    idk wt im doing imma just leave this herer first

--- classes.py
+++ classes.py
@@ -1,6 +1,4 @@
 import random
-import numpy as np
-
 
 ###########
 ## Classes
@@ -10,10 +8,20 @@ import numpy as np
 class Game:
     def __init__(self):
         self.players = [Player("E"),Player("S"),Player("W"),Player("N")]
-        self.played_tiles = {} ## dict or list???
-        self.drawable_tiles = {}
-        ## can add more stuff
-    
+        self.played_tiles = {} 
+        ## dict or list??? sld be dict {"tile",number of played tiles(0)}
+        ## 
+        self.drawable_tiles = {} ## {"tile", number of tiles(136)}
+        ## can add more stuff 
+        ## 
+
+        ## when drawing from drawable_tiles to player_tiles: --> drawable_tile - 1; player_tiles + 1
+        ## when player_discard: --> player_tiles - 1; played_tiles + 1
+        
+        ### drawable_tiles + played_tiles = 136
+        
+    def distribute(self):
+
     def reset(self):
         ## empty player_tiles, put all into drawable, shuffle
         pass
@@ -26,6 +34,8 @@ class Game:
     def deal(self):
         ## deals tiles to 4 players
         pass
+
+    def player_discard(self): 
     
     
     def __str__(self):
@@ -44,13 +54,13 @@ class Player:
         self.hand = Hand()
         self.wind = wind
 
-        
-    def draw(self):
-        # should add to self.hand
+
+    def draw(self):  #should add to self.hand
+        self.hand.add_tile(tile)
         pass
 
-    def discard(self):
-        # should remove from self.hand
+    def discard(self): # should remove from self.hand
+        self.hand.remove_tile(idx)
         pass
     
     def __str__(self):
@@ -62,17 +72,22 @@ class Hand:
     def __init__(self):
         self.tiles = []
 
-    def add_tile(self,tile):
-        ## add tile to self.tiles
+    def add_tile(self,tile): ## add tile to self.tiles
+        self.tiles.append(tile)
         pass
 
-    def remove_tile(self,idx):
-        ## removes tile at idx from self.tiles
+    def remove_tile(self,idx): ## removes tile at idx from self.tiles
+        self.tile.remove(tile)
         pass
 
-    def num_tiles(self):
-        ##return number of tiles in hand
+    def num_tiles(self): ##return number of tiles in hand
+        num_count = 0
+        for tile in self.tiles:
+            num_count +=1
+        return num_count
         pass
+
+
     def __str__(self):
         idx_string = ""
         tile_string = ""
@@ -83,10 +98,6 @@ class Hand:
             count+=1
         return idx_string + "\n" + tile_string
     
-
-
-
-
 
 
 
@@ -102,20 +113,19 @@ class Tile:
         Parameters:
         -------------
         type : str
-            stick, circle, million, words
+            stick (s), circle (c), million(m), words(w)
 
         value: int
             normal: 1-9
             words: 1:east, 2:south, 3:west, 4:north, 5:mid, 6:fat, 7:white
         """
-
         super().__init__() #inherit
-        if type not in ["stick", "circle", "million", "words"]:
+        if type not in ["s", "c", "m", "w"]:
             raise Exception("No such Mahjong type, got {}".format(self.type))
         self.type = type
         self.value = value
         self.word_dict = {1:"east", 2:"south", 3: "west", 4: "north", 5:"mid", 6:"fat",7: "white"}
-        if type != "words":
+        if type != "w":
             if value >9 or value <1:
                 raise Exception("Value not in range")
         else:
@@ -123,20 +133,26 @@ class Tile:
                 raise Exception("Value not in range")
 
 
-    def __str__(self):
-        if self.type == "words":
+    def __str__(self): 
+        if self.type == "w":
             return self.word_dict[self.value]
         else:
             return "{}{}".format(self.value, self.type)
 
     
-    def isDirection(self):
-        if self.type == "words":
+    def isWind(self): #identifying wind tiles 
+        if self.type == "w":
             if self.value < 5:
                 return True  
         return False
-    
-    
+
+    def isDragon(self): #identifying dragon tiles
+        if self.type == "w":
+            if self.value > 4:
+                return True
+        return False
+        
+
     def __eq__(self, other):
         if self.type == other.type and self.value == other.value:
             return True
--- game.py
+++ game.py
@@ -6,17 +6,10 @@ from classes import *
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    print(Tile("stick", 2))
-    print(Tile("words",6))
-    print(Tile("circle",1))
-    a = Tile("circle" , 6)
-    b = Tile("words", 3)
-    print(a.isDirection())
-    print(b.isDirection())
-    c = Tile("circle" , 6)
-    print(a==c)
-=======
+    hand = Hand()
+    tile = Tile("c", 6)
+    player = Player("aiko")
+    print(player)
     game = Game()
     print(game)
->>>>>>> 7e9ea8d4c1d85b1344e030334b7fc0f0ce54f88c
+     
